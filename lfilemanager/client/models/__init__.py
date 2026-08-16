6ed8298245f8749bbfdef17baf1eabb630a19195
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2026-08-15 19:46:45 -0500

    Feature: Corrections for correct use of tests

--- lfilemanager/client/models/__init__.py
+++ lfilemanager/client/models/__init__.py
@@ -2,5 +2,26 @@
 
 from .usuario import Usuario
 from .rol import Rol
+from .caso import Caso
+from .caso_normativa import CasoNormativa
+from .codigo_legal import CodigoLegal
+from .documento import Documento
+from .estado_caso import EstadoCaso
+from .notificacion import Notificacion
+from .pago import Pago
+from .plan import Plan
+from .tipo_caso import TipoCaso
 
-__all__ = ["Usuario", "Rol"]
+__all__ = [
+    "Usuario",
+    "Rol",
+    "Caso",
+    "CasoNormativa",
+    "CodigoLegal",
+    "Documento",
+    "EstadoCaso",
+    "Notificacion",
+    "Pago",
+    "Plan",
+    "TipoCaso",
+]
